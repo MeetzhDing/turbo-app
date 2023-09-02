857ccbf4c62af515e3947dd83bac36824771b0cb
MeetzhDing <meet.zhding@foxmail.com>
2023-09-02 14:26:08 +0800

    feat: upgrade deps

---
+++ packages/utility/src/typescript.ts
@@ -0,0 +1,29 @@
+/**
+ * https://www.typescriptlang.org/docs/handbook/utility-types.html
+ */
+// Awaited<Type>
+// Partial<Type>
+// Required<Type>
+// Readonly<Type>
+// Record<Keys, Type>
+// Pick<Type, Keys>
+// Omit<Type, Keys>
+// Exclude<UnionType, ExcludedMembers>
+// Extract<Type, Union>
+// NonNullable<Type>
+// Parameters<Type>
+// ConstructorParameters<Type>
+// ReturnType<Type>
+// InstanceType<Type>
+// ThisParameterType<Type>
+// OmitThisParameter<Type>
+// ThisType<Type>
+// Intrinsic String Manipulation Types
+// Uppercase<StringType>
+// Lowercase<StringType>
+// Capitalize<StringType>
+// Uncapitalize<StringType>
+
+/**
+ * https://github.com/sindresorhus/type-fest
+ */
